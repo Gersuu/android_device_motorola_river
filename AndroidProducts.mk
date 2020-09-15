8845421ffd9605f6cee1c29f6593960735efc852
Gersuu <62456969+Gersuu@users.noreply.github.com>
2020-09-15 19:28:57 -0300

    river: bringup legion 11

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_river.mk
+    $(LOCAL_DIR)/legion_river.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_river-eng \
-    lineage_river-user \
-    lineage_river-userdebug
+    legion_river-eng \
+    legion_river-user \
+    legion_river-userdebug
